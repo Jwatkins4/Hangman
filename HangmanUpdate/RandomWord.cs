d3c3286687cb9142c5530f5246bd35f60ac5efaf
Jamar Watkins <jamarwatkins@Jamars-MacBook-Pro.local>
2021-10-19 21:28:31 -0400

    first commit

---
+++ HangmanUpdate/RandomWord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HangmanUpdate
+{
+    public class RandomWord
+    {
+       
+            Random rnd = new Random();
+            string[] wordlist = new string[]
+            { "Patriots" , "Vikings","Chargers", "Panthers",
+            "Saints","Bengals", "Cardinals","Bears", "Texans"};
+
+            public string getRandom()
+            {
+                int index = rnd.Next(wordlist.Length);
+                return wordlist[index];
+            }
+    }
+    
+}
